0cb475fa6138a56f9c0c592e629d74d1b76e2908
zhangxintao <zhang_xintao@hotmail.com>
2014-11-11 08:49:24 +0800

    Initial project struc

---
+++ Users/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace Users.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var data = new Dictionary<string, object>();
+            data.Add("Placeholder", "Placeholder");
+            return View(data);
+        }
+	}
+}
