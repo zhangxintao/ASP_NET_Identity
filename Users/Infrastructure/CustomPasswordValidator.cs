f987a9fbf9a65038d5ae54865c8a1d3fddac68f4
zhangxintao <zhang_xintao@hotmail.com>
2014-11-16 17:59:28 +0800

    Base Implementation

---
+++ Users/Infrastructure/CustomPasswordValidator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.Identity;
+
+namespace Users.Infrastructure
+{
+    public class CustomPasswordValidator : PasswordValidator
+    {
+        public override async Task<IdentityResult> ValidateAsync(string pass)
+        {
+            var result = await base.ValidateAsync(pass);
+            if (pass.Contains("12345"))
+            {
+                var errors = result.Errors.ToList();
+                errors.Add("Passwords cannot contain numeric sequences");
+                result = new IdentityResult(errors);
+            }
+            return result;
+        }
+    }
+}
